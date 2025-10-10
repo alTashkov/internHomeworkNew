65b79b1ac8f8328358c6aa82255c586a47e43f39
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-10-10 15:40:37 +0300

    Add all exercises and new folder

---
+++ CohesionCoupling/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace CohesionCoupling
+{
+    public static class Utils
+    {
+        //split the class Utils to other
+        //classes that have strong cohesion
+        //and are loosely coupled internally.
+
+        //finished
+    }
+}
---
+++ InheritancePolymorphism/OffsiteCourse.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritancePolymorphism
+{
+    public class OffsiteCourse : Course
+    {
+        private string town;
+
+        public string Town 
+        { 
+            get
+            {
+                return town;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Invalid town type value!");
+                }
+                town = value;
+            }
+        }
+
+        public OffsiteCourse(string name) : base(name)
+        {
+            this.Name = name;
+            this.Students = new List<string>();
+        }
+
+        public OffsiteCourse(
+            string courseName, 
+            string teacherName) : base(courseName, teacherName)
+        {
+            this.Name = courseName;
+            this.TeacherName = teacherName;
+            this.Students = new List<string>();
+        }
+
+        public OffsiteCourse(
+            string courseName, 
+            string teacherName, 
+            IList<string> students) : base(courseName, teacherName, students)
+        {
+            this.Name = courseName;
+            this.TeacherName = teacherName;
+            this.Students = students;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder(base.ToString());
+
+            if (this.Town != null)
+            {
+                result.Append("; Town = ");
+                result.Append(this.Town);
+            }
+
+            result.Append(" }");
+            return result.ToString();
+        }
+    }
+}
