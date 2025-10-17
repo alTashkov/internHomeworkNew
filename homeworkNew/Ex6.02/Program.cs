5759b7a5f6a0a6dfafe282568fb2a93803efebe1
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-10-17 09:00:26 +0300

    Add exercises for HQC hw 6

---
+++ homeworkNew/Ex6.02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex6._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //part 1
+            Potato potato;
+            //... 
+
+            if (potato)
+            {
+                if (potato.HasBeenPeeled && potato.IsNotRotten)
+                {
+                    CookPotato(potato);
+                }
+            }
+
+            //part 2
+            bool xIsInRange = (x >= MIN_X) && (x <= MAX_X);
+            bool yIsInRange = (yIsInRange <= MIN_Y) && (y <= MAX_Y);
+
+            if (xIsInRange && yIsInRange && shouldVisitCell)
+            {
+                VisitCell();
+            }
+        }
+    }
+}
