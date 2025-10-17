ef15c2d9a17740f726ef9f2ecc6097386a653c9c
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-10-17 09:34:14 +0300

    Add non-refactored exercises of hw 9.1 and 6

---
+++ homeworkNew/ControlFlowStatementsLoops/Ex6.02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex6._02
+{
+    internal class Program
+    {
+        //part 1
+        Potato potato;
+//... 
+        if (potato != null)
+           if(!potato.HasNotBeenPeeled && !potato.IsRotten)
+            Cook(potato);
+
+        //part 2
+        if (x >= MIN_X && (x =< MAX_X && ((MAX_Y >= y && MIN_Y <= y) && !shouldNotVisitCell)))
+        {
+           VisitCell();
+        }
+
+}
+}
