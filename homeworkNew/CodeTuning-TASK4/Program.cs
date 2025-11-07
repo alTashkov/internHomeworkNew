e42a583c0e4fc7d2b4c95f555d298f22ec85b091
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-11-07 11:12:27 +0200

    Add rest of unedited exercises to sln

---
+++ homeworkNew/CodeTuning-TASK4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeTuning_TASK4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
